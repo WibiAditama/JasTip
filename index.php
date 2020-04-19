69e5b678306c47628e72a073189884425c269757
Wibi <wibiaditama90@gmail.com>
2020-04-19 19:03:34 +0700

    update

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 $server_key = "SB-Mid-server-T1fCmc7INFtBIcdHJRjNZV8K";
 
 $is_production = false;
-$api_url = $is_production ? 'https://app.midtrans.com/snap/v1/transaction' : 
-			'https://app.sandbox.midtrans.com/snap/v1/transaction';
+$api_url = $is_production ? 
+   'https://app.midtrans.com/snap/v1/transaction' : 
+   'https://app.sandbox.midtrans.com/snap/v1/transaction';
 
 if(!strpos($_SERVER['REQUEST_URI'], '/charge')){
   http_response_code(404);
@@ -21,19 +22,19 @@ header('Content-Type: application/json');
 
 $charge_result = chargeAPI($api_url, $server_key, $request_body);
 
-http_response($charge_result['http_code']);
+http_response_code($charge_result['http_code']);
 
 echo$charge_result['body']
 
 function chargeAPI($api_url, $server_key, $request_body){
   $ch = curl_init();
   $curl_options = array(
-    CURLOPT_URL => $api_url;
+    CURLOPT_URL => $api_url,
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_POST => 1,
     CURLOPT_HEADER => 0,
     CURLOPT_HTTPHEADER => array(
-       'Content-type: application/json',
+       'Content-Type: application/json',
        'Accept: application/json',
        'Authorization: Basic ' . base64_encode($server_key . ':')
     );
@@ -42,7 +43,7 @@ function chargeAPI($api_url, $server_key, $request_body){
   curl_setopt_array($ch, $curl_options);
   $result = array(
     'body' => curl_exec($ch),
-    'http_code' => curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    'http_code' => curl_getinfo($ch, CURLINFO_HTTP_CODE),
   );
   return $result;
 }
