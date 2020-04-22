ee0730b230a237109b484a246d1879d2be26ddf2
Wibi <wibiaditama90@gmail.com>
2020-04-23 01:32:12 +0700

    updateing

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Set your server key (Note: Server key for sandbox and production mode are different)
-$server_key = '<SB-Mid-server-T1fCmc7INFtBIcdHJRjNZV8K>';
+$server_key = 'SB-Mid-server-T1fCmc7INFtBIcdHJRjNZV8K';
 // Set true for production, set false for sandbox
 $is_production = false;
 
